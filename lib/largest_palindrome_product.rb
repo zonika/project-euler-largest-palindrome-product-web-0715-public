e3f947d89ab69082584878bdccf968ef07c8a404
zonika <zchodosh@conncoll.edu>
2015-10-12 12:52:17 -0400

    done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,20 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  prod = []
+  (100..999).to_a.reverse.each do |n|
+    (100..999).to_a.reverse.each do |m|
+      if palindrome?(n*m)
+        prod << n*m
+      end
+    end
+  end
+  prod.max
+end
+
+def palindrome?(n)
+  n = n.to_s.split('')
+  if n.length%2==0
+    n[0...n.length/2] == n[n.length/2...n.length].reverse
+  else
+    n[0...n.length/2] == n[n.length/2+1...n.length].reverse
+  end
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def answer
+    prod = []
+    (100..999).to_a.reverse.each do |n|
+      (100..999).to_a.reverse.each do |m|
+        if palindrome?(n*m)
+          prod << n*m
+        end
+      end
+    end
+    prod.max
+  end
+
+  def palindrome?(n)
+    n = n.to_s.split('')
+    if n.length%2==0
+      n[0...n.length/2] == n[n.length/2...n.length].reverse
+    else
+      n[0...n.length/2] == n[n.length/2+1...n.length].reverse
+    end
+  end
+end
